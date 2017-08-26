9b2cf2f0fcd568260640b99d55b1ae921f3ff459
HowardTangHw <2379034@qq.com>
2017-08-26 12:45:14 +0800

    2017/08/26

--- LeetCode/637. Average of Levels in Binary Tree.js
+++ LeetCode/637. Average of Levels in Binary Tree.js
@@ -24,34 +24,6 @@ var averageOfLevels = function(root) {
 
   add(root, i);
   function add(node, i) {
-    if (!node) {
-      if (sum[i] == undefined) {
-        sum[i] = 0;
-        count[i] = 0;
-      }
-      sum[i] += node.val;
-      count[i]++;
-      i++;
-    }
-    if (!node.left) add(node.left, i);
-    if (!node.right) add(node.right, i);
-  }
-  for (var j = 0, len = sum.length; j < len; j++) {
-    res.push(sum[j] / count[j]);
-  }
-  return res;
-};
-
-var averageOfLevels = function(root) {
-  if (!root) return;
-
-  var sum = [],
-    count = [],
-    res = [],
-    i = 0;
-
-  add(root, i);
-  function add(node, i) {
     if (node) {
       if (sum[i] === undefined) {
         sum[i] = 0;
