6fca7cf770b49ad1f260332ca3d256832d36f689
HowardTangHw <2379034@qq.com>
2017-09-07 18:23:46 +0800

    2017/09/07

--- LeetCode/617. Merge Two Binary Trees.js
+++ LeetCode/617. Merge Two Binary Trees.js
@@ -16,8 +16,8 @@
 var mergeTrees = function(t1, t2) {
  if(t1.val||t2.val)add(t1,t2);
   function add(n1,n2){
-    n1.val=n1.val?n1.val:0;
-    n2.val=n2.val?n2.val:0;
+    n1.val=n1?n1.val:0;
+    n2.val=n2?n2:0;
     n1.val=n2.val+n1.val;
     if((n1.left)||(n2.left)) add(n1.left,n2.left)
     if((n1.right)||(n2.right)) add(n1.right,n2.right)
