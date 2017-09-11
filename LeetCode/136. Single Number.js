d0fb3c558336ab30d3e678219d7bec3428d6b243
HowardTangHw <2379034@qq.com>
2017-09-11 09:27:43 +0800

    2017/09/11

--- LeetCode/136. Single Number.js
+++ LeetCode/136. Single Number.js
@@ -15,3 +15,18 @@ var singleNumber = function(nums) {
       return value != array[index - 1] && value != array[index + 1];
     });
 };
+
+/**
+ * 这道题中,只有一个是single Number,可以使用异或运算符
+ * 口诀：相同取0，相异取1
+ * 这道题中:
+ * 规则,a^b,a<b则相加, a=b则为0 a>b则相减
+ */
+
+var singleNumber = function(nums){
+  var res;
+  for(var i=0;i<nums.length;i++){
+    res ^= nums[i];
+  }
+  return res;
+}
