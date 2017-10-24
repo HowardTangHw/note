8350795d988909291685ddb1f43f7218e36391fd
HowardTangHw <2379034@qq.com>
2017-10-24 18:27:31 +0800

    2017/10/24

---
+++ LeetCode/202. Happy Number.js
@@ -0,0 +1,23 @@
+/**
+ * link:https://leetcode.com/problems/happy-number/description/
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  if (n <= 0) return false;
+  while (true) {
+    let sum = 0;
+    while (n != 0) {
+      sum += (n % 10) * (n % 10);
+      n = Math.floor(n / 10);
+    }
+    if (!Math.floor(sum / 10)) {
+      if (sum == 1 || sum == 7) return true;
+      else return false;
+    }
+    n = sum;
+  }
+};
