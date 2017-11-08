40bc25d9f9adda5b25d6c9442fb4c9f5c630ecdf
HowardTangHw <2379034@qq.com>
2017-11-08 18:37:07 +0800

    2017/11/08

---
+++ LeetCode/717. 1-bit and 2-bit Characters.js
@@ -0,0 +1,21 @@
+/**
+ * link:https://leetcode.com/problems/1-bit-and-2-bit-characters/description/
+ */
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */ 
+
+/**
+ * 如果是1,则加两位,因为1后面肯定是1或者是0的
+ * 如果是0,则自加0,再看看后一位是不是,
+ * 其实就是判断2 bits是否完整,完整且+0则是正确的
+ */
+var isOneBitCharacter = function(bits) {
+  let len = bits.length - 1;
+  let i = 0;
+  while (i < len) {
+    i += bits[i] + 1;
+  }
+  return i === len;
+};
