df8487c6fa9982c5bf0603f82a3874058399700c
HowardTangHw <2379034@qq.com>
2018-02-12 16:28:17 +0800

    210-470 commit

--- util/src/Array.js
+++ util/src/Array.js
@@ -207,4 +207,265 @@ const forEachRight = (arr, cb) =>
     .reverse()
     .forEach(cb);
 
+/**
+ * head
+ * 返回数组的第一个数(意义何在???)
+ * */
+
+const head = arr => arr[0];
+
+/**
+ * last
+ * 返回数组最后一个
+ * */
+
+const last = arr => arr[arr.length - 1];
+
+/**
+ * indexOfAll
+ * 查找元素的所有索引值
+ * 利用forEach & push
+ * */
+
+const indexOfAll = (arr, val) => {
+  const indices = [];
+  arr.forEach((v, i) => v == val && indices.push(i));
+  return indices;
+};
+
+/**
+ * initial
+ * 截取数组从0到length-1个数(就是不要最后那个了)
+ * */
+
+const initial = arr => arr.slice(0, -1);
+
+/**
+ * initialize2DArray
+ * 创建一个二维的数组
+ * */
+
+const initialize2DArray = (w, h, val = null) =>
+  Array.from({ length: h }).map(() => Array.from({ length: w }).fill(val));
+
+/**
+ * initializeArrayWithRange
+ * 按照输入的值,初始化数组(按照顺序)
+ * initializeArrayWithRange = (end, start = 0, step = 1)
+ *    end:结束为止
+ *    start:开始为止(默认为0)
+ *    step:递增的步数(可以为负数),默认为1
+ *    demo:
+ *          initializeArrayWithRange(5); // [0,1,2,3,4,5]
+ *          initializeArrayWithRange(7, 3); // [3,4,5,6,7]
+ *          initializeArrayWithRange(9, 0, 2); // [0,2,4,6,8]
+ *          initializeArrayWithRange(0, 9, -2); // [8,6,4,2,0]
+ * */
+
+const initializeArrayWithRange = (end, start = 0, step = 1) =>
+  Array.from({ length: Math.ceil((end + 1 - start) / step) }).map((v, i) => i * step + start);
+
+/**
+ * intersection
+ * 与difference相反,保留的是两个数组之间共有的
+ * */
+
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter(v => s.has(v));
+};
+
+/**
+ * intersectionBy
+ * 与differenceBy相反,
+ * 对比两个经过fn处理的数组,返回相同的元素
+ * 返回的是未经过fn处理的数据.
+ * */
+
+const intersectionBy = (a, b, fn) => {
+  const s = new Set(b.map(v => fn(v)));
+  return a.filter(v => s.has(fn(v)));
+};
+
+/**
+ *intersectionWith
+ * 返回arr 与val中,通过对比函数(comp)的数
+ * Example:
+ *         intersectionWith([1, 1.2, 1.5, 3, 0], [1.9, 3, 0, 3.9], (a, b) => Math.round(a) === Math.round(b)); // [1.5, 3, 0]
+ */
+
+const intersectionWith = (a, b, comp) => a.filter(x => b.findIndex(y => comp(x, y) !== -1));
+
+/**
+ * isSorted
+ * 利用了Array.entries()和for...of,取出键值
+ * 如果键是最后一个了,则返回direction
+ * 如果途中,有符合递增或递减的数
+ * 返回1为递增,返回-1为递减,返回0则没排序
+ * */
+
+const isSorted = arr => {
+  const direction = arr[0] > arr[1] ? -1 : 1;
+  for (let [i, v] of arr.entries())
+    if (i === arr.length - 1) return direction;
+    else if ((v - arr[i + 1]) * direction > 0) return 0;
+};
+
+/**
+ * join
+ * 将数组转为字符串拼接,还可以定义最后一个用什么符号...
+ * */
+
+const join = (arr, separator = ',', end = separator) =>
+  arr.reduce(
+    (acc, val, i) =>
+      i === arr.length - 2
+        ? acc + val + end
+        : i === arr.length - 1 ? acc + val : acc + val + separator,
+    ''
+  );
+
+/**
+ * longestItem
+ * 最长的元素
+ * */
+
+const longestItmen = (...vals) => [...vals].sort((a, b) => a.length - b.length)[0];
+
+/**
+ * mapObject
+ * 对数组进行fn处理,最终结果返回一个对象,对象的键为原始值,值为处理过的函数
+ * 转化为es5代码会更容易理解
+ * */
+
+//  ES5 代码
+var mapObject = function mapObject(arr, fn) {
+  // 自执行函数,最后返回的是一个对象,原因在下面
+  return (function(a) {
+    // 这里的a 是undefined,用它转化为数组,第一个值为arr,第二个值存放的是经过fn处理的值
+    // return如果有多个值,只保留后面那个,所以最终的返回值是一个经过reduce处理的对象{}
+    return (
+      (a = [arr, arr.map(fn)]),
+      // a[0]为原数组,
+      a[0].reduce(function(acc, val, ind) {
+        // 原始值作为键名,值为a[1]即经过fn处理的arr,并且要返回acc,原因同上
+        return (acc[val] = a[1][ind]), acc;
+      }, {})
+    );
+  })();
+};
+
+const mapObject = (arr, fn) =>
+  (a => (
+    (a = [arr, arr.map(fn)]), a[0].reduce((acc, val, i) => ((acc[val] = a[1][i]), acc), {})
+  ))();
+
+/**
+ * maxN
+ * 最大的N个值
+ * */
+
+const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
+
+/**
+ * minN
+ * 最小的N个值
+ * */
+
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
+
+/**
+ * nthElement
+ * 返回数组的第N个元素(意义不明...是因为处理了负数?)
+ * */
+
+const nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n)[0]);
+
+/**
+ * partition
+ * 分割,将数组分割,
+ * 分割的依据是每个元素通过fn处理,最后相同的值放在同一个数组里.
+ * */
+
+const partition = (arr, fn) =>
+  arr.reduce(
+    (acc, value, i, arr) => {
+      acc[fn(val, i, arr) ? 0 : 1].push(val);
+      return acc;
+    },
+    [[], []]
+  );
+
+/**
+ * pull
+ * 利用includes筛选剩下的值,然后给arr,
+ * 会对原数组进行变更
+ * Array.includes也可以用Set来代替的吧.
+ * */
+
+const pull = (arr, ...args) => {
+  let argState = Array.isArray(args[0]) ? args[0] : args;
+  let pulled = arr.filter((v, i) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach(v => arr.push(v));
+};
+
+/**
+ * pullAtIndex
+ * 和上面差不多,
+ * 根据输入的索引数组,移除相关下标的值
+ * removed存放的是要移除的值,pulled存放的是剩下的值
+ * */
+
+const pullAtIndex = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach(v => arr.push(v));
+  return removed;
+};
+
+/**
+ * pullAtValue
+ * 根据输入值,移除原数组内相同的值
+ * */
+
+//  自己根据上面的修改的
+// const pullAtValue = (arr, pullArr) => {
+//   let removed = [];
+//   let pulled = arr.map((v, i) => (pullArr.includes(v) ? (removed.push(v),v) : v)).filter((v, i) => !pullArr.includes(v));
+//   arr.length = 0;
+//   pulled.forEach(v => arr.push(v));
+//   return removed;
+// };
+
+const pullAtValue = (arr, pullArr) => {
+  let removed = [],
+    pushToRemove = arr.forEach((v, i) => (pullArr.includes(v) ? removed.push(v) : v)),
+    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
+  arr.length = 0;
+  mutateTo.forEach(v => arr.push(v));
+  return removed;
+};
+
+/**
+ * pullBy
+ * 原始数组,和对比数组,两者通过fn处理后,筛选出原始数组有,输入数组没有的数据
+ * 第一步,判断args中,最后一个参数是否为function,
+ * 第二步,用fn处理args
+ * 第三部,在已处理的args中,有没有fn(arr[v])的值
+ * */
+
+const pullBy = (arr, ...args) => {
+  const length = args.length;
+  let fn = length > 1 ? args[length - 1] : undefined;
+  fn = typeof fn === 'function' ? (args.pop(), fn) : undefined;
+  let argState = (Array.isArray(args[0]) ? args[0] : args).map(val => fn(val));
+  let pulled = arr.filter((v, i) => !argState.includes(fn(v)));
+  arr.length = 0;
+  pulled.forEach(v => arr.push(v));
+};
+
 export default { ary, call };
