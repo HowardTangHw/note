6212ae7859dea5febc7f2da16416219df71a6eaa
Howard Tang <2379034@qq.com>
2019-09-27 16:47:51 +0800

    2019/09/27

---
+++ LeetCode/Medium/46. Permutations.js
@@ -0,0 +1,18 @@
+// backtrack
+var permute = function(nums) {
+    const res = [];
+    backtrack(nums, res);
+    return res;
+};
+
+function backtrack(nums, res, n = 0) {
+    if (n === nums.length) {
+        res.push(nums.slice(0));
+        return;
+    }
+    for (let i = n; i < nums.length; i++) {
+        [nums[i], nums[n]] = [nums[n], nums[i]];
+        backtrack(nums, res, n + 1);
+        [nums[n], nums[i]] = [nums[i], nums[n]];
+    }
+}
