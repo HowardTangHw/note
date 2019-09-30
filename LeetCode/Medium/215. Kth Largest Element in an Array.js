032e7b2025a55f668565e938ee071fd3219a961a
Howard Tang <2379034@qq.com>
2019-09-30 16:22:24 +0800

    2019/09/30 add doc

--- LeetCode/Medium/215. Kth Largest Element in an Array.js
+++ LeetCode/Medium/215. Kth Largest Element in an Array.js
@@ -3,10 +3,11 @@
  * @param {number} k
  * @return {number}
  */
-// var findKthLargest = function(nums, k) {
-//     return nums.sort((a, b) => b - a)[k - 1];
-// };
+var findKthLargest = function(nums, k) {
+    return nums.sort((a, b) => b - a)[k - 1];
+};
 
+//  Quickselect
 var findKthLargest = function(nums, k) {
     sort(nums, 0, nums.length - 1, k);
     return nums[nums.length - k];
@@ -19,21 +20,23 @@ const sort = (arr, left, right, k) => {
     sort(arr, idx + 1, right, k);
 };
 
+// https://zh.wikipedia.org/wiki/%E5%BF%AB%E9%80%9F%E9%80%89%E6%8B%A9
+// 快速排序中，有一個子過程稱為分區，可以在線性時間裡將一個列表分為兩部分（left和right），分別是小於基準和大於等於基準的元素。
 const partition = (arr, left, right) => {
-    const pivotIdx = Math.floor(Math.random() * (right - left + 1) + left);
-    let idx = left,
-        pivot = arr[pivotIdx];
-    swap(arr, right, pivotIdx);
+    let pivotIndex = Math.floor(Math.random() * (right - left + 1) + left);
+    let pivotValue = arr[pivotIndex];
+    let idx = left;
+    swap(arr, pivotIndex, right); // move pivot to end;
     for (let i = left; i < right; i++) {
-        if (arr[i] < pivot) {
+        if (arr[i] < pivotValue) {
             swap(arr, i, idx);
             idx++;
         }
     }
-    swap(arr, right, idx);
-    return idx;
+    swap(arr, right, idx); // move pivot to final place
+    return idx; // 基准点,左边都比pivot小,右边比pivot大
 };
 
 const swap = (arr, i, j) => {
     [arr[i], arr[j]] = [arr[j], arr[i]];
-}; 
+};
