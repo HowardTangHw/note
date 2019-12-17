5c38d1fea80c72cd5acae377f3985ed294bca59a
Howard Tang <2379034@qq.com>
2019-12-17 17:21:25 +0800

    Update 3. Longest Substring Without Repeating Characters.js

--- LeetCode/Medium/3. Longest Substring Without Repeating Characters.js
+++ LeetCode/Medium/3. Longest Substring Without Repeating Characters.js
@@ -20,3 +20,23 @@ var lengthOfLongestSubstring = function(s) {
     }
     return res;
 };
+
+// 雙指針窗口
+var lengthOfLongestSubstring = function(s) {
+  let res = 0,
+    left = 0,
+    right = 0,
+    window = {};
+  while (right < s.length) {
+    let c1 = s[right];
+    window[c1] = (window[c1] || 0) + 1;
+    right++;
+    while (window[c1] > 1) {
+      let c2 = s[left];
+      window[c2]--;
+      left++;
+    }
+    res = Math.max(res, right - left);
+  }
+  return res;
+};
