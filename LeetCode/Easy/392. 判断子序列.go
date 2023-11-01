2928dcfd206853c2de7ae63423c404674c1cb426
Howard Tang <2379034@qq.com>
2023-11-01 16:41:47 +0800

    Create 392. 判断子序列.go

---
+++ LeetCode/Easy/392. 判断子序列.go
@@ -0,0 +1,14 @@
+func isSubsequence(s string, t string) bool {
+	n,m := len(s), len(t)
+	i,j := 0,0
+
+	for i<n && j<m {
+		if s[i] == t[j] {
+			i++;
+			j++;
+		}else{
+			j++;
+		}
+	}
+	return i == n;
+}
